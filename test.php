dffaf98242f110a155263114e58da1b6053614cf
yoichi okami <taiyou0514@gmail.com>
2018-04-03 08:15:44 +0900

    test

---
+++ test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+    <title>PHPテスト</title>
+</head>
+<body>
+
+<p>PHPのテストです。</p>
+
+<p>
+    <?php
+    echo "5 + 2.4 = ", 5 + 2.4, '<br />';
+    echo "5 + '8year' = ", 5 + '8year', '<br />';
+    echo "5 + '東京' = ", 5 + '東京';
+    ?>
+</p>
+
+</body>
+</html>ｐ
